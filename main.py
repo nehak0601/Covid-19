a7992453a2a96fb08015fb5ae5605e2b17678e60
neha <nehakumarivvit@gmail.com>
2020-05-24 11:28:31 +0530

    ADD

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 import pandas as pd
 import json
-con = create_engine('sqlite:///covid19.db') 
+import os
+
+
+script_dir = os.path.dirname(__file__)
+rel_path = "covid_data/covid19.db"
+abs_file_path = os.path.join(script_dir, rel_path)
+con = create_engine('sqlite:///' + abs_file_path) 
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     country = request.args.get('country', 'country')
     query = "select * from covid4"
     if country != 'country':
       query = "select * from covid4 where country='{}'".format(country)
+      update  ="select lastUpdate from covid4 where country='India'"
+    df_update=pd.read_sql(query, con)
+    # df_update["lastUpdate"] = pd.to_datetime(df_update["lastUpdate"]).dt.strftime('%Y-%m-%d')
     df = pd.read_sql(query, con)
     df_new = df[['country', 'confirmed', 'deaths', 'recovered']].groupby(['country'], as_index=False).sum()
     data_kpis = get_kpis(df_new)
     df_new = df_new.sort_values(['confirmed'], ascending=False).head(50)
     df_new = df_new.to_json(orient='records')
-    print(df_new)
-    return render_template("home.html", data=df, chart_data=df_new, data_kpis=data_kpis, list1=country_names(), handler={'btn': country_names}, mode=country)
+    return render_template("home.html", data=df, chart_data=df_new, data_kpis=data_kpis, country_list=country_names(), mode=country, update=df_update, handler=request)
 
 def get_kpis(df):
     title = ["Total Countries","Confirmed Cases", "Case Recovered", "Confirmed Deaths"]
@@ -38,12 +47,19 @@ def get_kpis(df):
 
 def country_names():
     df_country=pd.read_sql('select distinct country from covid4',con)
-    list1=df_country.values.tolist()
-    return list1
+    country_list=df_country.values.tolist()
+    return country_list
 
 @app.route("/details")
 def details():
-    return render_template("details.html")    
+    return render_template("details.html",handler=request) 
+
+@app.route("/faq")
+def FAQs():
+    query = "select * from covid6"
+    df=pd.read_sql(query, con)
+    return render_template("faq.html", data=df, handler=request)       
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+   app.run(host='0.0.0.0', debug=True, port=8888)
--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,38 @@
 $(document).ready(function(){
-		chart_['columns'] = ['country', 'confirmed', 'deaths', 'recovered'];
+    chart_['columns'] = ['country', 'confirmed', 'deaths', 'recovered'];
     if(mode == 'country') {
-		  render_multiline('#chart', chart_);	
+      render_multiline('#chart', chart_); 
   }
+
 });
 
 function render_multiline(selector, data){
-	// Set the dimensions of the canvas / graph
-	// Set the dimensions of the canvas / graph
-	//console.log(data)
-	var svg = d3.select(selector),
+ // $.each(data, function(index,value){
+ //  console.log(value.country)
+ // });
+
+  var svg = d3.select(selector),
         margin = {top: 20, right: 40, bottom: 100, left: 50},
         width = $(selector).width() - margin.left - margin.right,
         height = 350 - margin.top - margin.bottom;
 
     svg = svg.append('svg')
-    			.attr('width', "100%")
-    			.attr('height', height + margin.top + margin.bottom)
+          .attr('width', "100%")
+          .attr('height', height + margin.top + margin.bottom)
           .attr('viewBox', "0 0 " + ($(selector).width()) + " " + (height +  margin.bottom))
     var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var x = d3.scalePoint().range([0, width]),
         y = d3.scaleLinear();
         z = d3.scaleOrdinal()
-             .range(["green", "orange", "red"]);  
+             .range(['#008000', '#FFA500', '#FF0000']);  
+
 
     var line = d3.line()
         .x(function(d) { return x(d.country); })
         .y(function(d) { return y(d.expression); })
 
-      var znfs = data.columns.slice(1).map(function(id) {
+    var znfs = data.columns.slice(1).map(function(id) {
         return {
           id: id,
           values: data.map(function(d) {
@@ -37,23 +40,51 @@ function render_multiline(selector, data){
           })
         };
       });
-      console.log([d3.min(data, function (d) { return d.confirmed; }), d3.max(data, function (d) { return d.confirmed; })])
+
+     // console.log([d3.min(data, function (d) { return d.confirmed; }), d3.max(data, function (d) { return d.confirmed; })])
       x.domain(data.map(d=>d.country));
       y.domain([d3.min(data, function (d) { return d.confirmed; }), d3.max(data, function (d) { return d.confirmed; })]).range([height,0]);
       z.domain(znfs.map(function(c) { return c.name; }));
+      
+      //set legends
+      var legends = ["Confirmed Cases", "Recovered Cases","Total Death"];
+      var color = d3.scaleOrdinal()
+                    .range(['orange', 'green', 'red']);
+      var legend = svg.selectAll(".legend")
+        .data(legends)
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) {
+          return "translate(0," + i * 17 + ")";
+        });
+      
+        legend.append("rect")
+          .attr("x", width - (-70))
+          .attr("width", 12)
+          .attr("height", 12)
+          .style("fill", function(d, i){return color(i)});
+
+        legend.append("text")
+          .attr("x", width - (-65))
+          .attr("y", 5)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d, i) {
+            return d;
+          });
 
       g.append("g")
           .attr("class", "x axis")
           .attr("transform", "translate(0," + (height + 10) + ")")
           .style("font", "14px times")
-			    .call(d3.axisBottom(x))
-			    .selectAll("text")
-			    .attr("y", 0)
-			    .attr("x",-10)
-			    .attr("dy", ".35em")
-			    .attr("transform", "rotate(-65)")
-			    .style("text-anchor", "end")
-			   ;
+          .call(d3.axisBottom(x))
+          .selectAll("text")
+          .attr("y", 0)
+          .attr("x",-10)
+          .attr("dy", ".35em")
+          .attr("transform", "rotate(-65)")
+          .style("text-anchor", "end")
+         ;
       g.append("g")
           .attr("class", "axis axis--y")
           .call(d3.axisLeft(y))
@@ -68,25 +99,56 @@ function render_multiline(selector, data){
         .data(znfs)
         .enter().append("g")
         .attr("class", "city")
-        
+      var div = d3.select("body").append("div") 
+        .attr("class", "tooltip1")       
+        .style("opacity", 1);
 
       city.append("path")
           .attr("class", "line")
           .attr("d", function(d) { return line(d.values); })
-          .attr("title", function(d) { return d.id + ' : ' + data.map(d=>d.country) })   // Return tooltip value
           .style("stroke", function(d) { return z(d.id); })
-          .on("mouseover", function(d) {    
+          .on("mouseover", function(d, i) {    
                 div.transition()    
-                    .duration(200)    
-                    .style("opacity", .9);    
-                div .html(d.id)  
+                    .duration(0)    
+                    .style("opacity", 1);  
+                var mouse = d3.mouse(this);
+                var _filterdata = get_hovered_country(mouse);
+                div.html(get_tooltip(_filterdata))  
                     .style("left", (d3.event.pageX) + "px")   
                     .style("top", (d3.event.pageY - 28) + "px");  
                 })          
             .on("mouseout", function(d) {   
                 div.transition()    
-                    .duration(500)    
+                    .duration(2000)    
                     .style("opacity", 0); 
             });
-    $('[title]').tooltip();
+
+function get_hovered_country(mouse) {
+  var x_cordinate = parseInt(mouse[0]);
+  var pos = [], val = [];
+  $('.x.axis>.tick').each(function(i, node){
+      var legend = $('.x.axis>.tick').eq(i).text();
+      var x_pos = parseInt($(this).attr('transform').slice(10).split(',')[0]);
+      pos.push(Math.abs(parseInt(mouse) - x_pos));
+      val.push(legend);
+    })
+  var index = pos.indexOf(Math.min.apply(Math, pos));
+  var filterdata = data.filter(function(obj) {
+    return (obj.country === val[index]);
+  });
+  return filterdata
 }
+
+function get_tooltip(_filterdata) {
+  _html = "<table class='table-tooltip table'>";
+  _html += "<tr><td>Country</td><td>" + _filterdata[0]['country'] + "</td></tr>";
+  _html += "<tr><td>Comfirmed</td><td>" + _filterdata[0]['confirmed'] + "</td></tr>";
+   _html += "<tr><td>Recovered</td><td>" + _filterdata[0]['recovered'] + "</td></tr>";
+    _html += "<tr><td>Deaths</td><td>" + _filterdata[0]['deaths'] + "</td></tr>";
+  _html += "</table>"
+  return _html;
+}
+
+}
+
+
